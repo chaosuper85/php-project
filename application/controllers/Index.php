80d11586dccdc84bd4f1443564de87c2f3497694
root <root@10-10-53-151.(none)>
2015-09-26 10:24:02 +0800

    提交代码

---
+++ application/controllers/Index.php
@@ -0,0 +1,35 @@
+<?php
+
+	/* 首页 */
+	class IndexController extends Yaf_Controller_Abstract {
+
+		/* 自动调用，类似于构造方法 */
+		public function init(){}
+
+		/* 默认Action */
+		public function indexAction() {
+//			$userModel = new models_table(); //实例化一个数据库对象
+			//$userModel->getData(); //查询一条数据
+			//$userModel->getDataAll(); //查询多条数据
+			//$userModel->updateData(); //更新数据
+			//$userModel->deleteData(); //删除数据
+			//$userModel->insertData(); //插入数据
+			$str = 'Thanks a lot!!!';
+			$this->getView()->assign("content", $str);
+			//$this->getView()->display('index/index.html');
+		}
+        
+		/* 默认Action */
+		public function listAction() {
+			//$userModel = new models_table(); //实例化一个数据库对象
+			//$userModel->getData(); //查询一条数据
+			//$userModel->getDataAll(); //查询多条数据
+			//$userModel->updateData(); //更新数据
+			//$userModel->deleteData(); //删除数据
+			//$userModel->insertData(); //插入数据
+			$str = 'Thanks a lot2!!!';
+			$this->getView()->assign("content", $str);
+			//$this->getView()->display('index/index.html');
+		}
+	}
+?>
